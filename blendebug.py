75ee6e29cdf12b6d1258d97836cf1470ba0cd07e
Jorge Romeu Huidobro <jorge.romeu.huidobro@gmail.com>
2022-05-23 16:50:54 +0200

    Add colors

--- blendebug.py
+++ blendebug.py
@@ -1,198 +1,201 @@
 import bpy
 import random
+import numpy as np
 
 # CHANGE THINGS HERE
 LOGFILE = '/tmp/pbrt.INFO'
+
 SETTINGS = {
     'MAX_OBJS': 100,
-    'LIMIT_POINTS': True,
-    'LIMIT_LINES': True,
-    'LIMIT_DIRECTIONS': True,
-    'DIRECTION_LENGTH': 0.5
+    'DIRECTION_LENGTH': 0.5,
+    'COLLECTION': 'DEBUG',
 }
 
-STATE = {'N_OBJS': 0}
+COLORS = {
+    'BLACK': (0, 0, 0, 0),
+    'WHITE': (1, 1, 1, 1),
+    'RED': (1, 0, 0, 1),
+    'GREEN': (0, 1, 0, 1),
+    'BLUE': (0, 0, 1, 1),
+}
 
-def pointHandler(params):
-    global STATE
-    global SETTINGS
+# utilities
+def parse_point(point_str: str) -> np.ndarray:
+    """
+    Parse a 3D point in the format "[x, y, z]" to a numpy vector
+    :param point_str: point in string format
+    :return:
+    """
+    x, y, z = point_str[1:-2].split(',')
+    x = float(x)
+    y = float(y)
+    z = float(z)
+    return np.array([x, y, z])
 
-    if SETTINGS['LIMIT_POINTS'] and STATE['N_OBJS'] <= SETTINGS['MAX_OBJS']:
-        return
+def to_vertex(arr: np.ndarray):
+    """
+    Convert a numpy array to blender vertex data
+    :param arr: numpy array representing 3d point
+    :return: same point as a blender vertex
+    """
+    x, y, z = arr
+    return x, y, z
+
+def create_object(verts, edges, faces, name, color):
+    mesh = bpy.data.meshes.new(name)
+    mesh.from_pydata(verts, edges, faces)
+    mesh.validate()
+    mesh.update()
+
+    # create obj
+    obj = bpy.data.objects.new(name, mesh)
+    obj.color = COLORS[color]
+    bpy.data.collections[SETTINGS['COLLECTION']].objects.link(obj)
+
+# handlers
+def point_handler(params, color):
+    p = parse_point(params[0])
+
+    # if there is a second arg, use as name
+    try:
+        name = str(params[2])
+    except IndexError:
+        name = 'l'
+
+    verts = [p]
+    edges = []
+    faces = []
+
+    create_object(verts, edges, faces, name, color)
+
+def line_handler(params, color):
+    # parse first point
+    p0 = parse_point(params[0])
+    p1 = parse_point(params[1])
+
+    # if there is a second arg, use as name
+    try:
+        name = str(params[2])
+    except IndexError:
+        name = 'l'
+
+    verts = [to_vertex(p0), to_vertex(p1)]
+    edges = [(0, 1)]
+    faces = []
+
+    create_object(verts, edges, faces, name, color)
+
+def tri_handler(params, color):
+    # parse first point
+    p0 = parse_point(params[0])
+    p1 = parse_point(params[1])
+    p2 = parse_point(params[2])
+
+    # if there is an additional arg, use as name
+    try:
+        name = str(params[3])
+    except IndexError:
+        name = 'l'
+
+    verts = [to_vertex(p0), to_vertex(p1), to_vertex(p2)]
+    edges = [(0, 1), (0, 2), (1, 2)]
+    faces = []
+
+    create_object(verts, edges, faces, name, color)
+
+def tetra_handler(params, color):
+    # parse first point
+    p0 = parse_point(params[0])
+    p1 = parse_point(params[1])
+    p2 = parse_point(params[2])
+    p3 = parse_point(params[3])
+
+    # if there is an additional arg, use as name
+    try:
+        name = str(params[4])
+    except IndexError:
+        name = 'l'
+
+    verts = [to_vertex(p0), to_vertex(p1), to_vertex(p2), to_vertex(p3)]
+    edges = [(0, 1), (0, 2), (1, 2), (3, 0), (3, 1), (3, 2)]
+    faces = []
+
+    create_object(verts, edges, faces, name, color)
+
+def direction_handler(params, color):
+    origin = parse_point(params[0])
+    direction = parse_point(params[1])
+
+    # if third arg, name
+    try:
+        name = str(params[2])
+    except IndexError:
+        name = 'dir'
+
+    length = SETTINGS['DIRECTION_LENGTH']
+    arrowLen = length / 10
+
+    endpoint = origin + length * direction
+
+    # vector orthogonal to direction (for arrow heads)
+    direction_orth = np.array([-direction[1] * arrowLen, -direction[0] * arrowLen, 0])
+    arrow_l = endpoint - arrowLen * direction + direction_orth
+    arrow_r = endpoint - arrowLen * direction - direction_orth
+
+    verts = [to_vertex(origin), to_vertex(endpoint), to_vertex(arrow_l), to_vertex(arrow_r)]
+    edges = [(0, 1), (1, 2), (1, 3)]
+    faces = []
+
+    create_object(verts, edges, faces, name, color)
+
+HANDLERS = {'POINT': point_handler,
+            'LINE': line_handler,
+            'DIR': direction_handler,
+            'TRI': tri_handler,
+            'TETRA': tetra_handler}
+
+def parse_trace(trace: str):
+    pre, params = trace.split(':')
 
     try:
-        nums = params[0][1:-2]
-        x, y, z = nums.split(',')
-        x = float(x)
-        y = float(y)
-        z = float(z)
+        ty, color = pre.split('-')
+    except ValueError:
+        ty = pre
+        color = 'BLACK'
 
-        # if there is a second arg, use as name
-        try:
-            name = str(params[2])
-        except IndexError:
-            name = 'l'
+    params = params.split(';')
+    return ty, color, params
 
-        verts = [(x, y, z)]
-        edges = []
-        faces = []
-
-        # create mesh
-        mesh = bpy.data.meshes.new(name)
-        mesh.from_pydata(verts, edges, faces)
-        mesh.validate()
-        mesh.update()
-
-        # create obj
-        obj = bpy.data.objects.new(name, mesh)
-        collection.objects.link(obj)
-
-        STATE['N_OBJS'] += 1
-
-    except:
-        print(f'failed to plot point: {params}')
-
-def lineHandler(params):
-    global STATE
-    global SETTINGS
-
-    if SETTINGS['LIMIT_LINES'] and STATE['N_OBJS'] <= SETTINGS['MAX_OBJS']:
-        return
-
-    try:
-        # parse first point
-        p0 = params[0][1:-2]
-        x0, y0, z0 = p0.split(',')
-        x0 = float(x0)
-        y0 = float(y0)
-        z0 = float(z0)
-
-        # parse second point
-        p1 = params[1][1:-2]
-        x1, y1, z1 = p1.split(',')
-        x1 = float(x1)
-        y1 = float(y1)
-        z1 = float(z1)
-
-        # if there is a second arg, use as name
-        try:
-            name = str(params[2])
-        except IndexError:
-            name = 'l'
-
-        verts = [(x0, y0, z0), (x1, y1, z1)]
-        edges = [(0, 1)]
-        faces = []
-
-        # create mesh
-        mesh = bpy.data.meshes.new(name)
-        mesh.from_pydata(verts, edges, faces)
-        mesh.validate()
-        mesh.update()
-
-        # create object
-        obj = bpy.data.objects.new(name, mesh)
-        collection.objects.link(obj)
-
-        STATE['N_OBJS'] += 1
-
-    except:
-        print(f'failed to plot line: {params}')
-
-def directionHandler(params):
-    global STATE
-    global SETTINGS
-
-    if SETTINGS['LIMIT_DIRECTIONS'] and STATE['N_OBJS'] >= SETTINGS['MAX_OBJS']:
-        return
-
-    try:
-        # parse origin
-        origin = params[0][1:-2]
-        og_x, og_y, og_z = origin.split(',')
-        og_x = float(og_x)
-        og_y = float(og_y)
-        og_z = float(og_z)
-
-        # parse direction
-        direction = params[1][1:-2]
-        d_x, d_y, d_z = direction.split(',')
-        d_x = float(d_x)
-        d_y = float(d_y)
-        d_z = float(d_z)
-
-        # if third arg, name
-        try:
-            name = str(params[2])
-        except IndexError:
-            name = 'dir'
-
-        length = SETTINGS['DIRECTION_LENGTH']
-        arrowLen = length / 10
-
-        direction_orth = (-d_y * arrowLen, -d_x * arrowLen, 0)
-
-        origin = (og_x, og_y, og_z)
-        endpoint = (og_x + length * d_x, og_y + length * d_y, og_z + length * d_z)
-        arrow_l = (endpoint[0] - arrowLen * d_x + direction_orth[0], endpoint[1] - arrowLen * d_y + direction_orth[1],
-                   endpoint[2] - arrowLen * d_z + direction_orth[2])
-        arrow_r = (endpoint[0] - arrowLen * d_x - direction_orth[0], endpoint[1] - arrowLen * d_y - direction_orth[1],
-                   endpoint[2] - arrowLen * d_z - direction_orth[2])
-
-        verts = [origin, endpoint, arrow_l, arrow_r]
-        edges = [(0, 1), (1, 2), (1, 3)]
-        faces = []
-
-        # create mesh
-        mesh = bpy.data.meshes.new(name)
-        mesh.from_pydata(verts, edges, faces)
-        mesh.validate()
-        mesh.update()
-
-        # create object
-        obj = bpy.data.objects.new(name, mesh)
-        collection.objects.link(obj)
-
-        STATE['N_OBJS'] += 1
-
-    except:
-        print(f'failed to plot line: {params}')
-
-HANDLERS = {'POINT': pointHandler,
-            'LINE': lineHandler,
-            'DIR': directionHandler}
-
-if __name__ == '__main__':
-
-    # place all objects in the DEBBUG collection
-    collection = bpy.data.collections.get('DEBUG')
-
+def place_traces(collection_name: str):
     # if DEBUG collection doesnt exist, make one
-    if not collection:
-        collection = bpy.data.collections.new('DEBUG')
+    if not bpy.data.collections.get(collection_name):
+        collection = bpy.data.collections.new(collection_name)
         bpy.context.scene.collection.children.link(collection)
 
-    # clear the collection
+    # clear objects in DEBUG collection
     bpy.ops.object.select_all(action='DESELECT')
-    for object in collection.objects[:]:
-        object.select_set(True)
+    for obj in bpy.data.collections[collection_name].objects[:]:
+        obj.select_set(True)
     bpy.ops.object.delete()
 
-    # place all objects in debug file
-    lines = open(LOGFILE).readlines()
-
-    # get the debug-lines    
-    debuglines = []
-    for line in lines:
+    # filter log file for traces only
+    traces = []
+    for line in open(LOGFILE):
         if 'DBG' in line:
             _, data = line.split('DBG ')
-            debuglines.append(data)
+            traces.append(data)
 
-    random.shuffle(debuglines)
+    # shuffle and truncate traces (to only display a subset)
+    random.shuffle(traces)
+    traces = traces[0:SETTINGS['MAX_OBJS']]
 
-    # plot each structure
-    for line in debuglines:
-        ty, params = line.split(':')
-        params = params.split(';')
+    # plot the traces
+    for trace in traces:
 
-        HANDLERS[ty](params)
+        ty, color, params = parse_trace(trace)
+
+        # plot the trace
+        HANDLERS[ty](params, color)
+
+if __name__ == '__main__':
+    place_traces(SETTINGS['COLLECTION'])
